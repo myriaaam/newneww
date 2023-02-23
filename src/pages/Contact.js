56073d087901f0e8c4ce845578ecf946bd2ac367
mimi <myriambozak12@gmail.comgit config --global user.email myriambozak12@gmail.com  git config --global user.name mimigit config --global user.email myriambozak12@gmail.com>
2023-02-23 17:56:50 +0100

    code source

---
+++ src/pages/Contact.js
@@ -0,0 +1,16 @@
+
+
+import React from 'react'; 
+
+import Menu from '../Menu';
+
+function contact(props) {
+return (
+<div>
+    <Menu/>
+<h1>contact</h1>
+</div>
+);
+}
+
+export default contact;
---
+++ src/pages/Formation.js
@@ -0,0 +1,19 @@
+
+
+
+import React from 'react'; 
+
+
+import Menu from '../Menu'; 
+import Halo from './/car.js';
+function Formation(props) {
+return (
+<div>
+    <Menu/>
+<h1>formation</h1>
+<Halo />
+</div>
+);
+}
+
+export default Formation;
